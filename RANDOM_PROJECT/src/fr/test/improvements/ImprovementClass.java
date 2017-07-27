f32cd55649523416811284725a531f42b242804e
Alexandre <Alexandre@192.168.1.15>
2017-07-27 22:50:36 +0200

    imporvement

--- RANDOM_PROJECT/src/fr/test/improvements/ImprovementClass.java
+++ RANDOM_PROJECT/src/fr/test/improvements/ImprovementClass.java
@@ -4,5 +4,10 @@ public class ImprovementClass {
 
 	public static void main(String[] args) {
 		System.out.println("nothing is gonna be done here");
+		//this comment is going to cause a conflic
+		
+		/**
+		 * CONFLIC!!!
+		 */
 	}
 }
