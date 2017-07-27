df5a40fa5f013ee158750a73d6d3d934423aa953
Alexandre <Alexandre@192.168.1.15>
2017-07-27 22:51:21 +0200

    peace man

--- RANDOM_PROJECT/src/fr/test/improvements/ImprovementClass.java
+++ RANDOM_PROJECT/src/fr/test/improvements/ImprovementClass.java
@@ -4,5 +4,10 @@ public class ImprovementClass {
 
 	public static void main(String[] args) {
 		System.out.println("nothing is gonna be done here");
+		//no problem man
+		
+		/**
+		 * NO CONFLIC, PEACE~~
+		 */
 	}
 }
